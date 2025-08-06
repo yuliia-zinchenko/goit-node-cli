81b2649c30e976ed393483fadc4e8fbca2c11484
Yulia <julietta.zinchenko@gmail.com>
2025-08-06 12:57:00 +0300

    console app

---
+++ index.js
@@ -0,0 +1,47 @@
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "./src/contacts.js";
+
+import { program } from "commander";
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const list = await listContacts();
+      console.log(list);
+      break;
+
+    case "get":
+      const contact = await getContactById(id);
+      console.log(contact);
+      break;
+
+    case "add":
+      const addedContact = await addContact({ name, email, phone });
+      console.log(addedContact);
+      break;
+
+    case "remove":
+      const removedContact = await removeContact(id);
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
