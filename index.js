b86b43b814c8199c472eda3a311214225f682a0c
Yulia <julietta.zinchenko@gmail.com>
2025-08-06 20:16:17 +0300

    added modules not ES

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-import {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-} from "./src/contacts.js";
-
-import { program } from "commander";
+const contacts = require("./src/contacts");
+const { program } = require("commander");
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -20,22 +14,22 @@ const options = program.opts();
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      const list = await listContacts();
+      const list = await contacts.listContacts();
       console.log(list);
       break;
 
     case "get":
-      const contact = await getContactById(id);
+      const contact = await contacts.getContactById(id);
       console.log(contact);
       break;
 
     case "add":
-      const addedContact = await addContact({ name, email, phone });
+      const addedContact = await contacts.addContact({ name, email, phone });
       console.log(addedContact);
       break;
 
     case "remove":
-      const removedContact = await removeContact(id);
+      const removedContact = await contacts.removeContact(id);
       console.log(removedContact);
       break;
 
--- src/contacts.js
+++ src/contacts.js
@@ -1,28 +1,28 @@
-import fs from "fs/promises";
-import { nanoid } from "nanoid";
-import path from "path";
-import { fileURLToPath } from "url";
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+const path = require("path");
+// const { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 const rewriteContacts = (contacts) =>
   fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-export const listContacts = async () => {
+const listContacts = async () => {
   const contacts = await fs.readFile(contactsPath);
   return JSON.parse(contacts);
 };
 
-export const getContactById = async (contactId) => {
+const getContactById = async (contactId) => {
   const contacts = await listContacts();
   const contact = contacts.find((item) => item.id === contactId);
   return contact || null;
 };
 
-export const removeContact = async (contactId) => {
+const removeContact = async (contactId) => {
   const contacts = await listContacts();
   const index = contacts.findIndex((item) => item.id === contactId);
   if (index === -1) {
@@ -33,7 +33,7 @@ export const removeContact = async (contactId) => {
   return result;
 };
 
-export const addContact = async ({ name, email, phone }) => {
+const addContact = async ({ name, email, phone }) => {
   const contacts = await listContacts();
   const newContact = {
     id: nanoid(),
@@ -44,4 +44,11 @@ export const addContact = async ({ name, email, phone }) => {
   contacts.push(newContact);
   await rewriteContacts(contacts);
   return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
 };
